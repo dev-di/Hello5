359961006fe369c0ca1e11352d33802540c72352
dev-di <admin@masterin.se>
2017-07-20 16:49:33 +0200

    initial

---
+++ src/main/java/hello5/hello5/Hello5.java
@@ -0,0 +1,11 @@
+package hello5.hello5;
+
+public class Hello5 {
+	public static void main(String [] args) {
+		Hello5 hello = new Hello5();
+		System.out.println(hello.getMessage());
+	}
+	public String getMessage() {
+		return "Hello World?";
+	}
+}
